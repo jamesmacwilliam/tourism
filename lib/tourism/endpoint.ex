14fb464d32d9cb65c3b3f8df40bef7a40c51da0e
James Mac William <jimmy.macwilliam@gmail.com>
2017-07-26 08:12:45 -0400

    expire static assets in 1 day

--- lib/tourism/endpoint.ex
+++ lib/tourism/endpoint.ex
@@ -9,7 +9,8 @@ defmodule Tourism.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :tourism, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    cache_control_for_etags: "public, max-age=86400",
+    only: ~w(css fonts images js favicon.ico robots.txt bower_components)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
