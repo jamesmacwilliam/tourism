563bda9e8a492ac1a88c70af57263a17f2252b8c
James Mac William <jimmy.macwilliam@gmail.com>
2017-08-02 23:48:32 -0400

    Get polymer working with webpack
    - use babel-loader and polymer-webpack-loader
    - upgrade webpack to v3 + make config changes
    - add js maps to copy directive to ensure we do not get any missing file
    warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,39 @@
+const path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {'app': "./web/static/js/app.js", 'polymer': "./web/static/js/polymer.js"},
   output: {
-    path: "./priv/static/build",
+    path: path.resolve(__dirname, 'priv/static/build'),
     filename: "[name].js"
   },
   plugins: [
     new CopyWebpackPlugin([
       { from: './web/static/assets/images', to: '../images' },
+      { from: './web/static/assets/bower_components/**/*js.map', to: path.resolve(__dirname, 'priv/static/build/[name].map') },
       { from: './web/static/assets/favicon.ico', to: '../favicon.ico' },
       { from: './web/static/assets/robots.txt', to: '../robots.txt' }
     ])
   ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.css$/,
-        loaders: [
+        use: [
           'style-loader',
           'css-loader'
         ]
       },
       {
-        test: /\.(png|svg|jpg|gif|html)$/,
-        loader: 'file-loader'
+        test: /\.html$/,
+        use: [
+          'babel-loader',
+          'polymer-webpack-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: 'file-loader'
       }
     ]
   }
