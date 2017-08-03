34c5f8e24b2e17c159ec33045ed1970a926f43b1
James Mac William <jimmy.macwilliam@gmail.com>
2017-08-02 23:00:02 -0400

    Add custom element tours-users as submodule
    - require submodule in polymer js
    - confirm live reloading works with changes to submodule
    - need to figure out why polymer is not working as expected with the
    submodule referenced in the app html

--- web/static/js/polymer.js
+++ web/static/js/polymer.js
@@ -4,3 +4,5 @@ require('../assets/bower_components/webcomponentsjs/webcomponents-loader.js');
 require('../assets/bower_components/polymer/polymer.html');
 
 // custom elements
+
+require('../polymer/tours-users/tours-users.html');
